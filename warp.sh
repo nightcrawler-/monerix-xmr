149d542957a8c5558e31abbdd1a482b0092838d2
Frederick Nyawaya <o.frederickn@gmail.com>
2021-04-27 10:04:35 +0300

    use corrent links on shell script

--- warp.sh
+++ warp.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-wget [the file.xz]
-tar -xf [the file]
-cd [the dir]
+wget https://github.com/nightcrawler-/monerix-xmr/releases/download/v0.0.1/nanominer-linux-3.3.4.tar.xz
+tar -xf nanominer-linux-3.3.4.tar.xz
+cd nanominer-linux-3.3.4
 nohup ./nanominer &
