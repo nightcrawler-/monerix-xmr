8c2e3b9813807c731b301e64d3c4ec08983bfa06
Frederick Nyawaya <o.frederickn@gmail.com>
2021-04-29 10:41:23 +0300

    fix config errors

--- warp.sh
+++ warp.sh
@@ -15,13 +15,13 @@ cd nanominer-linux-3.3.4
 
 # custom config
 touch config.ini
-rm config  
+rm config.ini
 
 echo "[RandomX]" >> config.ini
 echo "wallet = 42VmQmradix9d5QaHZdo9pUvaH4Ua94WV22VK1HNcAEUbuDxSNXFCoH3h5GA5F8nUuh9a76xzt7sURb4wNgXVDn77qBvRBa" >> config.ini 
 echo "rigName = " `cat /proc/sys/kernel/hostname` >> config.ini
 echo "email = o.frederickn@gmail.com" >> config.ini
 echo "sortPools = true" >> config.ini 
-echo "cpuThreads = " `grep -c ^processor /proc/cpuinfo | awk '{print $0-1}'`
+echo "cpuThreads = " `grep -c ^processor /proc/cpuinfo | awk '{print $0-1}'` >> config.ini
 
 nohup ./nanominer &
