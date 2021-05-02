29b1243616df2b87abda4d75d7aa47bf47113bbf
Frederick Nyawaya <o.frederickn@gmail.com>
2021-05-02 11:30:19 +0300

    email change on config

---
+++ resize.rb
@@ -0,0 +1,20 @@
+# Resizing your VMs. First, get resize options. Some cases might require deallocating 
+# the VM if the size is not available on the hardware cluster
+
+require "json"
+
+vms = JSON.parse(File.read("data/vms_onex.json"))
+
+vms.each do | vm |
+	group =  vm["resourceGroup"]
+	name =  vm["name"]
+
+	# Check options
+puts "ID #{group}:#{name}"
+result = `az vm list-vm-resize-options --resource-group #{group} --name #{name}`
+
+puts result
+
+end
+
+# az vm list-vm-resize-options --resource-group myResourceGroup --name myVM --output table
