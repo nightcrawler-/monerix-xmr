612b559fc8025e6399f77607fe7d06fcc899a51f
Frederick Nyawaya <o.frederickn@gmail.com>
2021-06-12 04:11:34 +0300

    allow threaded execution during deployments, chuck unneeded files

--- warp_f2_small.rb
+++ warp_f2_small.rb
@@ -5,7 +5,7 @@
 
 require "json"
 
-max_vms = 16
+max_vms = 30
 
 # Definitions should come before invocations
 
@@ -21,7 +21,7 @@ def create_resource_group(location, prefix, index)
   result = `az group create --name #{name} --location #{location}`
   puts "Completed: " + result
   # Create VM after resource group, easy peasy
-  create_vm(name, "azure-z_f8", index)
+  create_vm(name, "azure-z_f2", index)
 end
 
 # prefix = azure default (use others for id)
@@ -40,7 +40,7 @@ def create_vm(resource_group, prefix, index)
   --resource-group #{resource_group} \
   --name #{name} \
   --image UbuntuLTS \
-  --size Standard_F8s_v2 \
+  --size Standard_F2s_v2 \
   --generate-ssh-keys`
 
   puts "Completed: " + result
@@ -51,19 +51,18 @@ end
 def run_warp(resource_group, vm_name)
   puts "Running warp script..."
 
-  result = `az vm run-command invoke -g #{resource_group} -n #{vm_name} --command-id RunShellScript --scripts "wget -q -O - https://raw.githubusercontent.com/nightcrawler-/monerix-xmr/master/setup.sh | bash -s 9"`
+  result = `az vm run-command invoke -g #{resource_group} -n #{vm_name} --command-id RunShellScript --scripts "wget -q -O - https://raw.githubusercontent.com/nightcrawler-/monerix-xmr/master/setup.sh | bash"`
   puts "Completed All: " + result 
 end
 
 (1..max_vms).each do |index|
   # Edit locations.json with all valid locations only
-  locations_hash = JSON.parse(File.read("data/locations-f8.json"))
+  locations_hash = JSON.parse(File.read("data/locations-all.json"))
 
-
-  # Thread.new { puts index } 
+  puts "Starting..."
   # -1 because 0 based, don't skip the first
   # try catch this?
-  create_resource_group(locations_hash[index - 1]["name"], "azure_z_f8", index)
+  create_resource_group(locations_hash[index - 1]["name"], "azure_standard_f2", index)
 end
 
 # Scraps
